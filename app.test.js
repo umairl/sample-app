91ce8df003d5c9a3fa4f9fb222c02059cf563dc5
Umair Latif Ch <163450390+umairl@users.noreply.github.com>
2025-07-04 20:18:00 +0500

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,6 +1,16 @@
 const request = require('supertest');
 const app = require('./app');
 
+let server;
+
+beforeAll((done) => {
+  server = app.listen(0, () => done()); // Random port for tests
+});
+
+afterAll((done) => {
+  server.close(done);
+});
+
 describe('GET /', () => {
   it('should return Hello World message', async () => {
     const res = await request(app).get('/');
